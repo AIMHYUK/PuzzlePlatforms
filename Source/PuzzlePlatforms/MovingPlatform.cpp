21db54af491904431c953bcd73237d10180fb3d0
Chojun <chojun917@gmail.com>
2024-03-26 20:55:18 +0900

    플랫폼 왕복

--- Source/PuzzlePlatforms/MovingPlatform.cpp
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -19,6 +19,9 @@ void AMovingPlatform::BeginPlay()
         SetReplicates(true);
         SetReplicateMovement(true);
     }
+
+    GlobalStartLocation = GetActorLocation(); //시작점
+    GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation); //끝점, 트랜스폼 액터를 가져와서 변환
 }
 void AMovingPlatform::Tick(float DeltaTime)
 {
@@ -26,11 +29,18 @@ void AMovingPlatform::Tick(float DeltaTime)
 
     if(HasAuthority())//true면 서버, false면 클라이언트
     {
-       	FVector Location = GetActorLocation();
-        FVector GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation); //트랜스폼 액터를 가져와서 변환
-        FVector Direction = (GlobalTargetLocation - Location).GetSafeNormal(); //벡터 정규화, 숫자가 작을때도 보호해줌
-	    Location += Speed * DeltaTime * Direction;
+        FVector Location = GetActorLocation();
+        float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size(); //움직일 거리
+        float JourneyTravelled = (Location - GlobalStartLocation).Size(); //움직인 거리
+        FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal(); //벡터 정규화, 숫자가 작을때도 보호해줌
+        Location += Speed * DeltaTime * Direction;
         SetActorLocation(Location); 
+	    if(JourneyTravelled >= JourneyLength ) //움직인 거리가 움직일 거리보다 커지면 시작점과 끝점 교환
+        {
+            FVector Swap = GlobalTargetLocation; 
+            GlobalTargetLocation = GlobalStartLocation;
+            GlobalStartLocation = Swap;
+        }       
     }
 }
 
--- Source/PuzzlePlatforms/MovingPlatform.h
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -24,4 +24,8 @@ public:
 	float Speed = 20;
 	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
 	FVector TargetLocation;
+
+private:
+	FVector GlobalTargetLocation;
+	FVector GlobalStartLocation;
 };
